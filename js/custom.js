96eaf8096897908eea1d0310375f51f32f7ca33f
Simon Barrett <simon@mightymango.ltd>
2018-11-27 11:07:23 +0000

    Added notification bar

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+//   Navbar
   var element = document.getElementById("navbar");
   var h = document.getElementById('navbar').clientHeight;
   if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
@@ -8,7 +9,15 @@ function scrollFunction() {
   } else {
     element.classList.remove("solid-navbar")
   }
+  
 }
+
+//   Notification
+  var notification = document.getElementById("notification")
+  if (notification) {
+	  var navbar = document.getElementById("navbar")
+	  navbar.classList.add("fixed-navbar-notification")
+  }
 
 $( document ).ready(function() {
   
